14206d528229506cbff6ca214394c8c3dc92466e
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-18 01:29:19 -0300

    connected Posts to Index

--- server/index.js
+++ server/index.js
@@ -3,9 +3,14 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 
 // Initializing app // 
 const app = express();
+
+
+
 
 
 // setting bodyParser to specify the requests //
@@ -13,12 +18,14 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use('/posts', postRoutes);
+
 
 // connecting with MongoDB Atlas database //
-const CONNECTION_URL = //'mongodb+srv://<user>:<password>@cluster0.nas2z.mongodb.net/?retryWrites=true&w=majority';//
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = 'mongodb+srv://<user>:<password>@cluster0.nas2z.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => console.log(error.message));
 

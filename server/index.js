d43332c9cf0e63c6f96da8e9569be7f653c4b0a2
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-18 01:11:10 -0300

    connect with MongoDB

--- server/index.js
+++ server/index.js
@@ -3,11 +3,24 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+
+// Initializing app // 
 const app = express();
 
+
+// setting bodyParser to specify the requests //
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 
+// connecting with MongoDB Atlas database //
+const CONNECTION_URL = 'mongodb+srv://manuel:sc86jaAuNs1pEUat@cluster0.nas2z.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
 

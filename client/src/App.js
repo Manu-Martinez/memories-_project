3843d3e2a466b7c29c5d57851daa808df3648bee
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-18 23:02:38 -0300

    Added png file to images folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+
+
 const App = () => {
     return (
         <Container maxWidth="lg">

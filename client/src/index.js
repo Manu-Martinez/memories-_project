a3dc33ab84909f0e64563f2212819637de63accb
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-20 00:22:35 -0300

    initializing redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+
 import App from './App';
 
+const store = createStore(compose(applyMiddleware(thunk)));
 
 ReactDOM.render( <App />, document.getElementById('root'));
 

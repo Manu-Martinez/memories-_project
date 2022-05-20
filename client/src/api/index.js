dfd39263bf8df2400b026cc4e6d55e9b53d41a84
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-20 16:23:05 -0300

    created POST method in api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,6 +4,10 @@ import axios from 'axios';
 //url pointing to the backend route //
 const url = 'http//:localhost:3000/posts';
 
+//GET method //
 export const fetchPosts = () => axios.get(url);
+
+// POST method//
+export const createPost = (newPost) => axios.post(url, newPost);
 
 

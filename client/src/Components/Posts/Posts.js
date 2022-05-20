a121ed98d48ede0e5d105397bf349d9e9784d607
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-20 20:49:45 -0300

    fixed minor issue in Posts

--- client/src/Components/Posts/Posts.js
+++ client/src/Components/Posts/Posts.js
@@ -6,8 +6,8 @@ import Post from './Post/Post';
 import useStyles from "./styles";
 
 const Posts = ({ setCurrentId }) => {
-    const posts = useSelector((state) => state.posts);
     const classes = useStyles();
+    const posts = useSelector((state) => state.posts);
 
 
     return (

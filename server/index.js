7ce629a3d7a802a887f8211a03194863575f1aea
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-21 01:35:51 -0300

    env file added

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 
 // Initializing app // 
 const app = express();
-
+dotenv.config();
 
 
 
@@ -22,8 +22,8 @@ app.use('/posts', postRoutes);
 
 
 // connecting with MongoDB Atlas database //
-const CONNECTION_URL = 'mongodb+srv://<user>:<password>@cluster0.nas2z.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+// const CONNECTION_URL = 'mongodb+srv://<user>:<password>@cluster0.nas2z.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 3000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))

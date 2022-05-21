a591db5be2dffb562c3dc9baa885cd318bde4e6f
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-21 00:38:08 -0300

    added LIKE request on the client side

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -11,7 +11,10 @@ export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 
 // PATCH route //
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id} `, updatedPost);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
 
 // DELETE route //
-export const deletePost = (id) => axios.delete(`${url}/${id} `);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
+
+// LIKE route //
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`); 

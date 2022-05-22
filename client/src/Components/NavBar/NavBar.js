f7538abf25a603d624be4d81c2edf3638c2b8f6b
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-21 23:17:25 -0300

    created NavBar component and added styles to it

---
+++ client/src/Components/NavBar/NavBar.js
@@ -0,0 +1,7 @@
+
+
+
+<AppBar className={classes.appBar} position="static" color="inherit">
+    <Typography className={classes.heading} variant="h2" align="center">Memories </Typography>
+    <img className={classes.image} src={memories} alt="memories" height="60" />
+</AppBar>

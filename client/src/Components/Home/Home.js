4854c1a1bdb797ef30e6bbb7e6fd45381722f1ab
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-22 16:13:53 -0300

    fixed error of classes being undefined

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -5,9 +5,11 @@ import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
 import { useDispatch } from "react-redux";
 import { getPosts } from '../../actions/posts';
+import { useStyles } from './styles';
 
 
 const Home = () => { 
+    const classes = useStyles();
     const { currentId, setCurrentId } = useState(null);
     const dispatch = useDispatch();
 
@@ -17,7 +19,7 @@ const Home = () => {
     return (
         <Grow in> 
         <Container>
-            <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid className={classes.gridContainer} container justify="space-between" alignItems="stretch" spacing={3}>
                 <Grid item xs={12} sm={7}>
                     <Posts  setCurrentId={setCurrentId}/> 
                 </Grid>

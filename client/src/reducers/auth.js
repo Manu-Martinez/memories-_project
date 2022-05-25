6786cd4ce902284c6c9cab57f61cda309bd7425d
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-25 14:50:48 -0300

    added AUTH and LOGOUT actions

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,1 +1,13 @@
 import { AUTH, LOGOUT } from '../constants/actionTypes';
+
+const authReducer = (state = { authData: null }, action) => {
+    switch (action.type) {
+        case AUTH:
+            localStorage.setItem('profile', JSON.stringify({ ...action?.data })); // saving data in localStorage so the browser will know you are still logged in //
+            return { ...state, authData:action?.data  };
+        default:
+            return state;
+    }
+};
+
+export default authReducer;

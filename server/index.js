dbfc7ec52b407640ce4ab838bd6efde80911d652
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-24 23:42:12 -0300

    fixed the server

--- server/index.js
+++ server/index.js
@@ -25,10 +25,9 @@ app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://manuel:manuel123@cluster0.nas2z.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 3000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => console.log(error));
 
-//mongoose.set('useFindAndModify', false);
 
 

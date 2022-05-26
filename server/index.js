b17e249aac34ceb9de338cf39e1a3f1255797c42
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-26 01:07:28 -0300

    added SignIn and SignUp in the backend

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 
 // Initializing app // 
@@ -19,7 +20,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
+app.use('/user', userRoutes);
 
 // connecting with MongoDB Atlas database //
 const CONNECTION_URL = 'mongodb+srv://manuel:manuel123@cluster0.nas2z.mongodb.net/?retryWrites=true&w=majority';

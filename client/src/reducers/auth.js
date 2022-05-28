483d9b9f123355773550cfc966d3508e078b718a
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-28 01:37:19 -0300

    fixed issue with authReducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,11 +1,11 @@
-import { AUTH, LOGOUT } from '../constants/actionTypes';
+import * as actionType from '../constants/actionTypes';
 
 const authReducer = (state = { authData: null }, action) => {
     switch (action.type) {
-        case AUTH:
+        case actionType.AUTH:
             localStorage.setItem('profile', JSON.stringify({ ...action?.data })); // saving data in localStorage so the browser will know you are still logged in //
-            return { ...state, authData: action?.data  };
-        case LOGOUT:
+            return { ...state, authData: action.data  };
+        case actionType.LOGOUT:
             localStorage.clear();
             return { ...state, authData: null };
         default:

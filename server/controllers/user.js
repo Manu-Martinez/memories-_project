75418af6f3666937bf912df16c499ee6f4bf04b1
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-28 02:11:25 -0300

    fixed issues with User controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,13 +1,15 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
-import user from '../models/user.js';
+import UserModal from '../models/user.js';
+
+const secret = 'test';
 
 export const signin = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const existingUser = await User.findOne({ email });
+        const existingUser = await UserModal.findOne({ email });
 
         if(!existingUser) return res.status(404).json({ message: "User doesn't exist" });
         
@@ -15,7 +17,7 @@ export const signin = async (req, res) => {
 
         if(!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
 
-        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1h" });
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, secret,  { expiresIn: "1h" });
 
         res.status(200).json({ result: existingUser, token });
     } catch (error) {
@@ -28,7 +30,7 @@ export const signup = async (req, res) => {
     const { email, password, confirmPassword, firstName, lastName } = req.body;
 
     try {
-        const existingUser = await User.findOne({ email });
+        const existingUser = await UserModal.findOne({ email });
 
         if(existingUser) return res.status(400).json({ message: "User already exists" });
 
@@ -36,9 +38,9 @@ export const signup = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 12);
 
-        const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+        const result = await UserModal.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
 
-        const token = jwt.sign({ email: result.email , id: result._id }, 'test', { expiresIn: "1h" });
+        const token = jwt.sign({ email: result.email , id: result._id }, secret, { expiresIn: "1h" });
 
         res.status(200).json({ result, token });
     } catch (error) {

909db9248f6eff61cf3dbe49b0f2e6ff94a74a14
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-28 01:42:26 -0300

    ordered reducers according to action

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -2,16 +2,17 @@ import { CREATE, UPDATE, DELETE, FETCH_ALL, LIKE } from '../constants/actionType
 
 export default (posts = [], action) => {
     switch (action.type) {
-        case DELETE:
-            return posts.filter((post) => post._id !== action.payload);   
-        case UPDATE: // stacked both UPDATE and LIKE because they do the same process //
-        case LIKE:
-            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         case FETCH_ALL:
             return action.payload;
+        case LIKE:
+            return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
         case CREATE:
             return [...posts, action.payload];
+        case UPDATE:
+            return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+        case DELETE:
+            return posts.filter((post) => post._id !== action.payload);   
         default:
             return posts;
     }
-}
+};

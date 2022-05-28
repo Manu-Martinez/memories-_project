d8f530aa889417ddde04dbaa19aece0a91232cf6
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-28 02:20:02 -0300

    fixed problem with User route

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,9 @@
 import express from "express";
-
 import { signin, signup } from '../controllers/user.js';
 
 const router = express.Router();
 
-router.post('/signin');
-router.post('/signup');
+router.post('/signin', signin);
+router.post('/signup', signup);
 
 export default router;

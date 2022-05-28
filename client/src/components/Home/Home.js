1c2985d18008af28ce5a95aa0f22db3970c58381
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-28 17:37:57 -0300

    brought back original files

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,33 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React, {useState, useEffect } from "react";
+import {Container, Grow, Grid} from '@material-ui/core';
 
+import Posts from "../Posts/Posts";
+import Form from "../Form/Form";
+import { useDispatch } from "react-redux";
 import { getPosts } from '../../actions/posts';
-import Posts from '../Posts/Posts';
-import Form from '../Form/Form';
+import useStyles from './styles';
 
-const Home = () => {
-  const [currentId, setCurrentId] = useState(0);
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
+const Home = () => { 
+    const classes = useStyles();
+    const [currentId, setCurrentId] = useState(null);
+    const dispatch = useDispatch();
 
-  return (
-    <Grow in>
-      <Container>
-        <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
-          <Grid item xs={12} sm={7}>
-            <Posts setCurrentId={setCurrentId} />
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <Form currentId={currentId} setCurrentId={setCurrentId} />
-          </Grid>
-        </Grid>
-      </Container>
+    useEffect(() => {
+    dispatch(getPosts()); }, [currentId, dispatch]);
+
+    return (
+        <Grow in> 
+        <Container>
+            <Grid className={classes.gridContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                <Grid item xs={12} sm={7}>
+                    <Posts setCurrentId={setCurrentId}/> 
+                </Grid>
+                <Grid item xs={12} sm={7}>
+                    <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                </Grid>
+            </Grid>
+        </Container> 
     </Grow>
-  );
+    );
 };
 
 export default Home;

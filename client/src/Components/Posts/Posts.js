3db8f05029f5f5ba45c91667e756601720516f0f
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-28 01:30:03 -0300

    fixed indentation problem

--- client/src/Components/Posts/Posts.js
+++ client/src/Components/Posts/Posts.js
@@ -11,18 +11,17 @@ const Posts = ({ setCurrentId }) => {
 
 
     return (
-         !posts.length ? <CircularProgress /> : (
-             <Grid className={classes.container} container alignItems='stretch' spacing={3} >
-                 {posts.map((post) => (
-                   <Grid key={post.id} item xs={12} sm={6}>
-                      <Post post={post} setCurrentId={setCurrentId} />
-                   </Grid>
-                 ))}
-             </Grid>
-         )
-
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems='stretch' spacing={3} >
+                {posts.map((post) => (
+                <Grid key={post._id} item xs={12} sm={6} md={6}>
+                    <Post post={post} setCurrentId={setCurrentId} />
+                </Grid>
+            ))}
+            </Grid>
+        )
     );
-}
+};
 
 export default Posts;
 

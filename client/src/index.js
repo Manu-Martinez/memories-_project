34ffa632cc981ff698ee7b0222e20153e5cfd90a
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-28 01:49:22 -0300

    fixed typing errors

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-import reducers from './reducers';
+import { reducers } from './reducers';
 
 
 import App from './App';
@@ -15,5 +15,6 @@ const store = createStore(reducers, compose(applyMiddleware(thunk)));
 ReactDOM.render( 
 <Provider store={store}>
     <App />
-</Provider>, document.getElementById('root'));
+</Provider>, document.getElementById('root'),
+);
 

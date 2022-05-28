b0828af304c3be94ad082e75d1504fb62b143449
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-28 01:47:54 -0300

    fixed typing errors

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import NavBar from "./components/NavBar/NavBar";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
 
@@ -11,13 +11,13 @@ import Auth from "./components/Auth/Auth";
 const App = () => (
   <BrowserRouter> 
     <Container maxWidth="lg"> 
-      <NavBar />
+      <Navbar />
       <Switch>
-        <Route path="/" exact component={Home}></Route>
-        <Route path="/auth" exact component={Auth}></Route>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
       </Switch>
     </Container>
   </BrowserRouter>
-)
+);
 
 export default App;

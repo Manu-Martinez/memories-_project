6d285e37604fc6c27376083a2e1a9dfd909ddf26
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-29 03:05:36 -0300

    added the Search controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -17,6 +17,21 @@ export const getPosts = async (req, res) => {
     }
 }
 
+// SEARCH POSTS //
+export const getPostsBySearch = async (req, res) => {
+    const { searchQuery, tags } = req.query;
+
+    try {
+        const title = new RegExp(searchQuery, 'i'); // flag i is used to ignore uppercase or lowercase //
+
+        //find all the posts that match the title or the tags //
+        const posts = await postMessage.find({ $or: [ { title }, { tags: { $in: tags.split(',') } } ]});
+
+        res.json({ data: posts });
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
 
 // GET SINGLE POST //
 

9e41b432c821b2585aa6ae2d1234af1b1f0826b7
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-29 01:49:33 -0300

    refactored App component

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,12 @@ import Auth from "./components/Auth/Auth";
 
 
 
-const App = () => (
-  <BrowserRouter> 
+const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'));
+
+
+  return (
+    <BrowserRouter> 
     <Container maxWidth="xl"> 
       <Navbar />
       <Switch>
@@ -18,10 +22,11 @@ const App = () => (
         <Route path="/posts" exact component={Home} />
         <Route path="/posts/search" exact component={Home} />
         <Route path="/posts/:id" component={PostDetails} />
-        <Route path="/auth" exact component={Auth} />
+        <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
       </Switch>
     </Container>
   </BrowserRouter>
-);
+  );
+};
 
 export default App;

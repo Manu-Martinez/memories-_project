685e68bd8529115c45ce49b3da4a5fda3b42e1d2
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-29 02:08:44 -0300

    added searchBar to Home component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect } from "react";
-import {Container, Grow, Grid, Paper} from '@material-ui/core';
+import {Container, Grow, Grid, Paper, AppBar, TextField, Button } from '@material-ui/core';
 import Pagination  from "../Pagination";
+import { useHistory, useLocation } from "react-router-dom";
+import ChipInput from '@material-ui-chip-input';
 
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
@@ -8,23 +10,42 @@ import { useDispatch } from "react-redux";
 import { getPosts } from '../../actions/posts';
 import useStyles from './styles';
 
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
+
 
 const Home = () => { 
     const classes = useStyles();
     const [currentId, setCurrentId] = useState(null);
     const dispatch = useDispatch();
+    const query = useQuery();
+    const history = useHistory();
+    const page = query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
 
     useEffect(() => {
-    dispatch(getPosts()); }, [currentId, dispatch]);
+    dispatch(getPosts()); 
+    }, [currentId, dispatch]);
 
     return (
         <Grow in> 
-        <Container>
-            <Grid className={classes.gridContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                <Grid item xs={12} sm={7}>
+        <Container maxWidth="xl">
+            <Grid container justifyContent="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
+                <Grid item xs={12} sm={6} md={9}> 
                     <Posts setCurrentId={setCurrentId}/> 
                 </Grid>
-                <Grid item xs={12} sm={7}>
+                <Grid item xs={12} sm={6} md={3}>
+                <AppBar className={classes.appBarSearch} position="static" color="inherit">
+                    <TextField 
+                    name="search" 
+                    variant="outlined" 
+                    label="Search Memories"
+                    fullWidth
+                    value="TEST"
+                    onChange={() => {}}
+                    />
+                </AppBar>
                     <Form currentId={currentId} setCurrentId={setCurrentId} />
                     <Paper className={classes.pagination} elevation={6}>
                         <Pagination />

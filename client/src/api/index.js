a6d4ed2b53dd95c33e7b37e34f62c445c46531e5
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-29 16:54:55 -0300

    added page parameter to the fetchPosts endpoint

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,14 +5,14 @@ import axios from 'axios';
 const API = axios.create({ baseURL: 'http://localhost:3000' });
 
 API.interceptors.request.use((req) => {
-    if(localStorage.getItem('profile')) {
+    if (localStorage.getItem('profile')) {
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
     }
     return req;
 });
 
 //GET endpoint //
-export const fetchPosts = () => API.get('/posts');
+export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
 
 
 // SEARCH endpoint //

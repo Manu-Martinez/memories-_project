11f515004c3473775bf32c962923497c60785c01
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-30 16:01:27 -0300

    added demo CommentSection to PostDetails

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -2,9 +2,11 @@ import React, { useEffect }  from 'react';
 import { Paper, Typography, CircularProgress, Divider } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
+
 import { useParams, useHistory } from 'react-router-dom';
 import useStyles from './styles';
 import { getPost, getPostsBySearch } from '../../actions/posts'; 
+import CommentSection from './CommentSection';
 
 
 const PostDetails = () => {
@@ -52,7 +54,7 @@ const PostDetails = () => {
                     <Divider style={{ margin: '20px 0' }} />
                     <Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography>
                     <Divider style={{ margin: '20px 0' }} />
-                    <Typography variant="body1"><strong>Comments - coming soon!</strong></Typography>
+                    <CommentSection post={post} />
                     <Divider style={{ margin: '20px 0' }} />
                 </div>
                 <div className={classes.imageSection}>

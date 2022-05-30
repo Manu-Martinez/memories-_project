79ac76f4311ae1d24da7ba4685d981043de9cc76
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-30 15:49:47 -0300

    added basic logic to CommentSection

---
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState, useRef } from "react";
+import { Typography, Textfield, Button } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+
+const CommentSection = () => {
+    console.log('Comment Section');
+    
+    return (
+        <h1>Comment Section</h1>    
+    );
+};
+
+export default CommentSection;

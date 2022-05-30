c9e05a8373863a7d7a60ac97ace7d7aeaa7ea896
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-30 01:08:36 -0300

    added Loading reducers and changed state array to object

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,7 +1,15 @@
-import { CREATE, UPDATE, DELETE, FETCH_ALL, LIKE, FETCH_BY_SEARCH } from '../constants/actionTypes';
+import { CREATE, UPDATE, DELETE, FETCH_ALL, LIKE, FETCH_BY_SEARCH, START_LOADING, END_LOADING } from '../constants/actionTypes';
 
-export default (state = [], action) => {
+export default (state = { isLoading: true, posts: [] }, action) => {
     switch (action.type) {
+        case START_LOADING:
+            return {
+                ...state, isLoading: true
+            };
+        case END_LOADING:
+            return {
+                ...state, isLoading: false
+            };
         case FETCH_ALL:
             return {
                 ...state,
@@ -15,13 +23,13 @@ export default (state = [], action) => {
                 posts: action.payload,
             };
         case LIKE:
-            return state.map((post) => (post._id === action.payload._id ? action.payload : post));
+            return { ...state, posts: state.map((post) => (post._id === action.payload._id ? action.payload : post)) };
         case CREATE:
-            return [...state, action.payload];
+            return { ...state, posts: [...state, action.payload] };
         case UPDATE:
-            return state.map((post) => (post._id === action.payload._id ? action.payload : post));
+            return { ...state, posts: state.map((post) => (post._id === action.payload._id ? action.payload : post)) };
         case DELETE:
-            return state.filter((post) => post._id !== action.payload);   
+            return { ...state, posts: state.filter((post) => post._id !== action.payload) };   
         default:
             return state;
     }

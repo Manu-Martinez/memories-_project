15d81c507c10f183466fc4883cfee336c3e7ac86
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-30 16:00:49 -0300

    added test comments

--- client/src/components/PostDetails/CommentSection.jsx
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -2,13 +2,28 @@ import React from "react";
 import { useState, useRef } from "react";
 import { Typography, Textfield, Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+import useStyles from "./styles";
 
 
-const CommentSection = () => {
-    console.log('Comment Section');
-    
+const CommentSection = ({ post }) => {
+    console.log(post);
+    const classes = useStyles();
+    cosnt [comments, setComments] = useState([1, 2, 3, 4]);
+
     return (
-        <h1>Comment Section</h1>    
+        <div>
+            <div className={classes.commentsOuterContainer}>
+                <div className={classes.commentsInnerContainer}>
+                    <Typography gutterbottom variant="h6" >Comments</Typography>
+                    {comments.map((c, i) => (
+                        <Typography key={i} gutterbottom variant="subtitle1" >
+                            Comment {i}
+                        </Typography>
+                    ))}
+                </div>
+            </div>
+        </div>
+
     );
 };
 

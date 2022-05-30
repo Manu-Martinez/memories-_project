104dd4c33401bc5b795ca2be5c7446ba1ac21cfa
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-05-30 01:56:58 -0300

    added FetchPost on the import

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,4 @@
-import { CREATE, UPDATE, DELETE, LIKE, FETCH_ALL, FETCH_BY_SEARCH, START_LOADING, END_LOADING } from '../constants/actionTypes';
+import { CREATE, UPDATE, DELETE, LIKE, FETCH_ALL, FETCH_POST, FETCH_BY_SEARCH, START_LOADING, END_LOADING } from '../constants/actionTypes';
 import * as api from '../api/index';
 
 
